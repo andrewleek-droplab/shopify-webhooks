c99e063c2b3b62ec4b70a1182293b83386b3dfde
Andrew Leek <andrewleek@gmail.com>
2021-02-05 02:09:11 +0100

    Removed requirement of setting requestType parameter
    Added some logic so that the request type is figured out automatically.

--- src/register-webhook.ts
+++ src/register-webhook.ts
@@ -2,24 +2,13 @@ import { ApiVersion, Topic, TopicGraphQL } from "./types";
 import { registerWebhookREST } from "./register-webhook-rest";
 import { registerWebhookGraphQL } from "./register-webhook-graphql";
 
-type OptionsREST = {
-	topic: Topic;
-	requestType: "rest";
-};
-
-type OptionsGraphQL = {
-	topic: TopicGraphQL;
-	requestType: "graphql";
-};
-
-type OptionsRequestType = OptionsREST | OptionsGraphQL;
-
 type Options = {
 	address: string;
 	accessToken: string;
 	shop: string;
 	apiVersion: ApiVersion;
-} & OptionsRequestType;
+	topic: string;
+};
 
 function isTopicREST(topic: string): topic is Topic {
 	return topic.toLowerCase() === topic;
@@ -31,23 +20,20 @@ function isTopicGraphQL(topic:string): topic is TopicGraphQL {
 
 export async function registerWebhook(options: Options): Promise<{ success: boolean; result: any; }> {
 	const {
-		requestType,
 		topic,
 		...sharedParams
 	} = options;
 
-	switch (requestType) {
-		case "rest":
-			if (!isTopicREST(topic)) {
-				throw new Error("Topic is not a REST API compatible topic");
-			}
-
-			return registerWebhookREST({ ...sharedParams, topic });
-		case "graphql":
-			if (!isTopicGraphQL(topic)) {
-				throw new Error("Topic is not a REST API compatible topic");
-			}
-
-			return registerWebhookGraphQL({ ...sharedParams, topic })
+	if (isTopicREST(topic))
+	{	
+		return registerWebhookREST({ ...sharedParams, topic });
+	}
+	else if (isTopicGraphQL(topic))
+	{
+		return registerWebhookGraphQL({ ...sharedParams, topic })
+	}
+	else
+	{
+		throw new Error("Topic is not a REST API or GraphQL API compatible topic");
 	}
 }
